a73b81c3d599f6e76cc165dd05ba914e4d1f2e51
abdula277 <gripanov@gmail.com>
2017-11-14 12:47:48 +0300

    Fix charts and add chart library to resources

--- src/ru/gpanov/Charts.java
+++ src/ru/gpanov/Charts.java
@@ -1,48 +1,35 @@
 package ru.gpanov;
 
-import javax.swing.JFrame;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
 
 public class Charts {
     public static void main(String[] args) {
-        XYSeries series = new XYSeries("sin(a)");
         int[] n = {8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40};
-        int[] d = {4, 4, 4, 4, 5, 6, 6, 7, 8, 7, 8, 8, 8, 8, 8, 9, 10};
-        for (int i = 0; i < n.length; i++) {
-            series.add(n[i], fu(n[i], d[i], 0.01));
-            System.out.println(String.format("x = %d, y = %f", n[i], fu(n[i], d[i], 0.01)));
-        }
+        int[] d = {4, 4,  4,  4,  5,  6,  6,  7,  8,  7,  8,  8,  8,  8,  8,  9,  10};
+        double[] errs = {0.1, 0.01, 0.001};
 
-        XYDataset xyDataset = new XYSeriesCollection(series);
-        JFreeChart chart = ChartFactory
-                .createXYLineChart("y = sin(x)", "x", "y",
-                        xyDataset,
-                        PlotOrientation.VERTICAL,
-                        true, true, true);
-        JFrame frame =
-                new JFrame("MinimalStaticChart");
-        // Помещаем график на фрейм
-        frame.getContentPane()
-                .add(new ChartPanel(chart));
-        frame.setSize(400, 300);
-        frame.show();
+        XYChart chart = new XYChartBuilder().width(800).height(600).title("title").xAxisTitle("n").yAxisTitle("err").build();
+
+        for(double err: errs) {
+            double[] x = new double[n.length];
+            double[] y = new double[n.length];
+            for (int i = 0; i < n.length; i++) {
+                x[i] = n[i];
+                y[i] = fu(n[i], d[i], err);
+            }
+            chart.addSeries(err + "", x, y);
+        }
+        chart.getStyler().setYAxisLogarithmic(true);
+        new SwingWrapper(chart).displayChart();
     }
 
-    static double fu(int n, int d, double p) {
-        int t = (d - 1) / 2;
-        double secondArg = 0;
-        for (int i = 0; i <= t; i++) {
-            secondArg += Utils.binomialCoefficient(n, i) * Math.pow((1 - p), i) * Math.pow(p, n - i);
+    private static double fu(double n, int d, double prob) {
+        double fx = 0;
+        for (int j = d  / 2; j <= n + 1; j++) {
+            fx += Utils.binomialCoefficient(n, j) * Math.pow((1 - prob), n - j) * Math.pow(prob, j);
         }
-        return 1 - secondArg;
+        return fx;
     }
-
-
 }
